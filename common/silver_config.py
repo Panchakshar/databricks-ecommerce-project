44a65c855b6138201f81d047f76a0e942d11e896
spanchakshar@gmail.com <spanchakshar@gmail.com>
2026-08-05 14:57:20 +0000

    Completed silver_config

--- common/silver_config.py
+++ common/silver_config.py
@@ -16,9 +16,10 @@ def agg_geolocations(df, spark):
 SILVER_RULES = {
     'customers' : {
         'dedup_keys' : ['customer_id'],
-        'pre_transform' : lambda df, spark: df.withColumn('customer_city',F.trim(F.lower(F.col('customer_city')))),
+        'pre_transform' : [
+            lambda df, spark: df.withColumn('customer_city',F.trim(F.lower(F.col('customer_city'))))
+        ],
         'rules' : [
-            (F.col('customer_id').isNull(),'missing_customer_id'),
             (F.col('customer_unique_id').isNull(),'missing_customer_unique_id'),
             (F.col('customer_zip_code_prefix').isNull(),'missing_customer_zip_code_prefix')
         ]
@@ -29,17 +30,12 @@ SILVER_RULES = {
         'pre_transform' : [
             agg_geolocations,
             lambda df,spark : df.withColumn('geolacation_city',F.trim(F.lower(F.col('geolocation_city'))))
-        ],
-        'rules' : [
-            (F.col('geolocation_zip_code_prefix').isNull(),'missing_zip_code')
         ]
     },
 
     'order_items' : {
         'dedup_keys' : ['order_id','order_item_id'],
         'rules' : [
-            (F.col('order_id').isNull(),'missing_order_id'),
-            (F.col('order_item_id').isNull(),'missing_order_item_id'),
             (F.col('price') <= 0, 'non_positivea_price'),
             (F.col('product_id').isNull(),'missing_product_id'),
             (F.col('seller_id').isNull(),'missing_seller_id')
@@ -47,27 +43,52 @@ SILVER_RULES = {
     },
 
     'order_payments' : {
-        'dedup_keys' : ['order_id','payment_sequntial'],
-        'pre_transform' : lambda df, spark: df.withColumn('payment_type',F.trim(F.lower(F.col('payment_type')))),
+        'dedup_keys' : ['order_id','payment_sequential'],
+        'pre_transform' : [
+            lambda df, spark: df.withColumn('payment_type',F.trim(F.lower(F.col('payment_type'))))
+        ],
         'rules' : [
-            (F.col('order_id').isNull(),'missing_order_id'),
             (F.col('payment_value') <= 0, 'non_positive_payment_value')
         ]
     },
 
     'order_reviews' : {
         'dedup_keys' : ['review_id'],
-        'pre_transform' : lambda df,spark : df.withColumn('review_comment_message',F.trim(F.lower(F.col('review_comment_message')))).withColumn('review_comment_title',F.trim(F.lower(F.col('review_comment_title')))),
+        'pre_transform' : [
+            lambda df,spark : df.withColumn('review_comment_message',F.trim(F.lower(F.col('review_comment_message')))).withColumn('review_comment_title',F.trim(F.lower(F.col('review_comment_title'))))
+        ],
         'rules' : [
-            (F.col('review_id').isNull(),'missing_review_id'),
             (F.col('order_id').isNull(),'missing_oder_id'),
             (~F.col('review_score').between(1,5),'invalid_review_score')
         ]
     },
 
-    'orders' 
+    'orders' : {
+        'dedip_keys' : ['order_id'],
+        'pre_transform' : [
+            lambda df,spark : df.withColumn('order_status',F.trim(F.lower(F.col('order_status'))))
+        ],
+        'rules' : [
+            (F.col('order_purchase_timestamp').isNull(),'missing_order_purchase_timestamp'),
+            (F.col('order_delivered_customer_date') < F.col('order_purchase_timestamp'), 'delivery_before_purchase')
+        ]
+    },
+
+    'products' : {
+        'dedup_keys' : ['product_id'],
+        'pre_transform' : [
+            lambda df,spark : df.withColumn('product_category_name',F.trim(F.lower(F.col('product_category_name')))),
+        translate_product_category
+    ]
+    },
+
+    'sellers' : {
+        'dedup_keys' : ['seller_id'],
+        'pre_transform' : [
+            lambda df,spark : df.withColumn('seller_city',F.trim(F.lower(F.col('seller_city'))))
+        ],
+        'rules' : [
+            (F.col('seller_zip_code_prefix').isNull(),'missing_seller_zip_code_prefix')
+        ]
+    }
 }
-
-
-
-
