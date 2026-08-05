ed0a644ac4603db8e3b41bd6b67628e26f1499ca
spanchakshar@gmail.com <spanchakshar@gmail.com>
2026-08-05 16:01:37 +0000

    Fixed typo in products schema

--- common/schema.py
+++ common/schema.py
@@ -61,7 +61,7 @@ products_schema = StructType([
     StructField('product_name_lenght',IntegerType(),True),
     StructField('product_description_length',IntegerType(),True),
     StructField('product_photos_qty',IntegerType(),True),
-    StructField('product_weight_q',IntegerType(),True),
+    StructField('product_weight_g',IntegerType(),True),
     StructField('product_length_cm',IntegerType(),True),
     StructField('product_height_cm',IntegerType(),True),
     StructField('product_width_cm',IntegerType(),True),
