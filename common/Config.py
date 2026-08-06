5f78f8680257bcdccd38c9dc9ba6a1e256ccbbe6
spanchakshar@gmail.com <spanchakshar@gmail.com>
2026-08-06 09:06:11 +0000

    created common config notebook

---
+++ common/Config.py
@@ -0,0 +1,11 @@
+catalog = 'ecommerce_lakehouse'
+
+raw_schema = 'raw'
+bronze_schema = 'bronze'
+silver_schema = 'silver'
+gold_schema = 'gold'
+
+raw_volume_path = f'/Volumes/{catalog}/{raw_schema}/data'
+
+dim_start_date = '2016-01-01'
+dim_end_date = '2020-12-31'
